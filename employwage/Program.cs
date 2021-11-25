3244965e096dde142e1bab4486d54a47fa37dd39
“sarthak <sarthakgangrade0@gmail.com>
2021-11-25 16:59:27 +0530

    employwage ability to save total wage for each company

---
+++ employwage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employwage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dmart = new EmpWageBuilderObject("Dmart", 20, 50, 10);
+            EmpWageBuilderObject Amazon = new EmpWageBuilderObject("Amazon", 10, 60, 20);
+            dmart.ComputeEmpWage();
+            Console.WriteLine(dmart.ToString());
+            Amazon.ComputeEmpWage();
+            Console.WriteLine(Amazon.ToString());
+            //Console.ReadLine();
+        }
+    }
+}
